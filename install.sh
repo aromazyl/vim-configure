69420bf2159ef42d1c4941976956b216401b2ff5
yule <dustymansion@163.com>
2017-02-10 16:18:22 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+# download ctags
+wget https://nchc.dl.sourceforge.net/project/ctags/ctags/5.8/ctags-5.8.tar.gz
+tar -xf ctags-5.8.tar.gz
+
+# install ctags
+cd ctags-5.8 && ./configure --prefix=$HOME/local/ctags/ && make && make install
+echo 'export PATH=$PATH:$HOME/local/ctags/bin/' >> ~/.bashrc
+cd ..
+
+#download vundle and install
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/
+vim -E -c BundleInstall -c q
